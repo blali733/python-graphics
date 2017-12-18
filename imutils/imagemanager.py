a9fa2b57d7eb68504d698c5ff2ad15b35b9c77c0
Michał Urbaniak <blali733@gmail.com>
2017-12-18 14:23:40 +0100

    Successfull image loading.

--- imutils/imagemanager.py
+++ imutils/imagemanager.py
@@ -4,6 +4,11 @@ import cv2
 
 
 class ImageManager:
-    def readImage(fileName):
+    def readImage(self, fileName):
         file = cv2.imread(fileName, 0)
         return file
+
+    def showImage(self, image, title):
+        cv2.imshow(title, image)
+        cv2.waitKey(0)
+        cv2.destroyAllWindows()
