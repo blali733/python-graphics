329317f62f5453610a50c6c696981d03a440894b
blali733 <blali733@gmail.com>
2017-12-18 19:20:01 +0100

    plug-in imagecrop

--- analyze.py
+++ analyze.py
@@ -1,16 +1,19 @@
 import sys  #for command line arguments
 from imutils import imagemanager
+from imutils import imagecrop
 
 
 class Analyze:
     def run(self, argv):
         print("I am the main now!")
         imageManager = imagemanager.ImageManager()
+        imageCrop = imagecrop.ImageCrop()
         image = imageManager.readImage('brain.jpg')
-        threshold, image = imageManager.binearizeImage(image, 20)
-        contours = imageManager.findContours(image)
+        threshold, imagebin = imageManager.binearizeImage(image, 20)
+        contours = imageManager.findContours(imagebin)
         newContours = imageManager.contours2angdist(contours, image.shape[0]/2, image.shape[1]/2)
-        imageManager.choosePointsForExpectingContour(newContours, 200)
+        finContours = imageManager.choosePointsForExpectingContour(newContours, 200)
+        image2 = imageCrop.excludeBackground(finContours,image)
         imageManager.showImage(image, "title")
 
 
