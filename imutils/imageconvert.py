37fa8c6712db655eeaffd3d8a437fe886b118917
Michał Urbaniak <blali733@gmail.com>
2018-01-09 20:40:59 +0100

    Prepared slicing stub

--- imutils/imageconvert.py
+++ imutils/imageconvert.py
@@ -1,7 +1,23 @@
 from medpy.io import load
+import matplotlib.pyplot as plt
+import matplotlib.cm as cm
+import numpy as np
+import cv2
 
 
 class ImageConvert:
     def main(self):
-        i, h = load("../data/raw/img/657_2_1.img")
-        print(h)
+        pass
+
+    def prepareSlices(self, path, image, spacing=1):
+        i, h = load(path+"/"+image)
+        mmax = i.max()
+        mmin = i.min()
+        for a in range(i.shape[0]):
+            for b in range(i.shape[1]):
+                for c in range(i.shape[2]):
+                    i[a, b, c] = (i[a, b, c] - mmin) / mmax * 255
+        # k = i[27, :, :]
+        i = np.uint8(i)
+        for j in range(int(i.shape[0] / spacing)):
+            cv2.imwrite("data/raw/out/" + image + (j*spacing).__str__() + ".jpg", i[j * spacing, :, :])
