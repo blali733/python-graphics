107411307ad20079252f6eb8f50825f63108c062
Michał Urbaniak <blali733@gmail.com>
2018-01-18 17:42:27 +0100

    Cleanup of math functions refactoring.

--- imutils/mha/mhaMath.py
+++ imutils/mha/mhaMath.py
@@ -1,29 +1,15 @@
 import numpy as np
 
 
-# def med_image_binearize(med_image_slice, level=0, dtype=np.uint8):
-#     """
-#     Function binearizing nparrays
-#
-#     Parameters
-#     ----------
-#     med_image_slice : nparray
-#         image to be binearized
-#     level : int
-#         level at which binearization would be performed; default 0
-#     dtype : npdatatype
-#         data type of resulting array; default np.uint8
-#
-#     Returns
-#     -------
-#         binearized image as nparray
-#     """
-#     temp = np.zeros(med_image_slice.shape, dtype=dtype)
-#     for x in range(med_image_slice.shape[0] - 1):
-#         for y in range(med_image_slice.shape[1] - 1):
-#             if med_image_slice[x, y] > level:
-#                 temp[x, y] = 1
-#     return temp
+def med_2_uint8(med_image_slice):
+    max_val = med_image_slice.max()
+    max_val = max(max_val, 1)
+    temp = np.zeros(med_image_slice.shape, dtype=np.float)
+    for x in range(med_image_slice.shape[0] - 1):
+        for y in range(med_image_slice.shape[1] - 1):
+            temp[x, y] = med_image_slice[x, y]/max_val
+    return temp
+
 
 def med_image_binearize(med_image_slice, level=0):
     """
