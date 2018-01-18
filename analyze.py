dd8ee736213d391557902b1b1cf30083104d9052
Michał Urbaniak <blali733@gmail.com>
2018-01-18 15:15:21 +0100

    Cleanup of old garbage.

--- analyze.py
+++ analyze.py
@@ -58,18 +58,6 @@ class Analyze:
                 pass
 
 
-# imageManager = imagemanager.ImageManager()
-#         imageCrop = imagecrop.ImageCrop()
-#         image = imageManager.readImage('brain.jpg')
-#         threshold, imagebin = imageManager.binearizeImage(image, 25)
-#         # cv2.imshow("rrr", imagebin)
-#         contours = imageManager.findContours(imagebin)
-#         newContours = imageManager.contours2angdist(contours, image.shape[0]/2, image.shape[1]/2)
-#         finContours = imageManager.choosePointsForExpectingContour(newContours, 100, 7.5)
-#         image2 = imageCrop.excludeBackground(finContours,image)
-#         threshold, image2 = imageManager.binearizeImage(image2, 180)
-#         imageManager.showImage(image2, "title")
-
 if __name__ == "__main__":
     app = Analyze()
     app.run(sys.argv)
