928cc76ac9dfb6c9b63012de768f76fe7ed5ad92
Michał Urbaniak <blali733@gmail.com>
2018-01-20 20:33:11 +0100

    Removed shading of basic function by variable name.

--- pimutils/segmentation/segment.py
+++ pimutils/segmentation/segment.py
@@ -1,17 +1,17 @@
 import numpy as np
 
 
-def flair(slice):
-    return np.zeros(slice.shape, dtype=slice.dtype)
+def flair(image_slice):
+    return np.zeros(image_slice.shape, dtype=image_slice.dtype)
 
 
-def t1(slice):
-    return np.zeros(slice.shape, dtype=slice.dtype)
+def t1(image_slice):
+    return np.zeros(image_slice.shape, dtype=image_slice.dtype)
 
 
-def t1c(slice):
-    return np.zeros(slice.shape, dtype=slice.dtype)
+def t1c(image_slice):
+    return np.zeros(image_slice.shape, dtype=image_slice.dtype)
 
 
-def t2(slice):
-    return np.zeros(slice.shape, dtype=slice.dtype)
+def t2(image_slice):
+    return np.zeros(image_slice.shape, dtype=image_slice.dtype)
