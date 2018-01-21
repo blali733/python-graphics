b250500cc267d1dc1c751da4c54a5a052bcafcdc
Unknown <l.prajzler@gmail.com>
2018-01-21 23:23:37 +0100

    xd

--- pimutils/mha/mhaMath.py
+++ pimutils/mha/mhaMath.py
@@ -3,7 +3,7 @@ import numpy as np
 def med_2_uint8(med_image_slice):
     max_val = med_image_slice.max()
     max_val = max(max_val, 1)
-    temp = np.zeros(med_image_slice.shape, dtype=np.float)
+    temp = np.zeros(med_image_slice.shape, dtype=np.int16)
     for x in range(med_image_slice.shape[0] - 1):
         for y in range(med_image_slice.shape[1] - 1):
             temp[x, y] = med_image_slice[x, y]/max_val
--- threshold_test.py
+++ threshold_test.py
@@ -7,9 +7,9 @@ from pimutils.mha import mhaMath
 slices = segmentationTest.flair_slices
 for slice in slices:
     img = mhaMath.med_2_uint8(slice)
-    ret, th = cv2.threshold(img, 127, 255, cv2.THRESH_BINARY)
-    th3 = cv2.adaptiveThreshold(img, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, 11, 2)
+    blur = cv2.GaussianBlur(img, (5, 5), 0)
+    ret1, th = cv2.threshold(blur, 127, 255, cv2.THRESH_BINARY)
     image = [th]
     for im in image:
-        print(im)
-        plt.imshow(im.reshape(im.shape[0], im.shape[1]), cmap=plt.cm.Greys)
+        #print(im)
+        plt.imshow(im, cmap=plt.cm.Greys)
