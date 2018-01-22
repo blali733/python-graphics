b43f6d5a5603cccf9c218b25d750d6f78c93c229
Michał Urbaniak <blali733@gmail.com>
2018-01-22 14:51:56 +0100

    Implemented 3 dimensional mask rebuilding. UNTESTED

--- pimutils/mask/recreate.py
+++ pimutils/mask/recreate.py
@@ -1,5 +1,26 @@
 import numpy as np
 
 
-def create_3d_array(list, axis):
-    pass
+def create_3d_array(layers_list, axis):
+    # TODO check if it couldn't be optimized by usage of numpy functions
+    dtype = layers_list[0].dtype
+    shape = layers_list[0].shape
+    if axis == 0:
+        array_3d = np.zeros((layers_list.__len__(), shape[0], shape[1]), dtype=dtype)
+        layer_id = 0
+        for layer in layers_list:
+            array_3d[layer_id, :, :] = layer
+            layer_id += 1
+    elif axis == 1:
+        array_3d = np.zeros((shape[0], layers_list.__len__(), shape[1]), dtype=dtype)
+        layer_id = 0
+        for layer in layers_list:
+            array_3d[:, layer_id, :] = layer
+            layer_id += 1
+    else:
+        array_3d = np.zeros((shape[0], shape[1], layers_list.__len__()), dtype=dtype)
+        layer_id = 0
+        for layer in layers_list:
+            array_3d[:, :, layer_id] = layer
+            layer_id += 1
+    return array_3d
