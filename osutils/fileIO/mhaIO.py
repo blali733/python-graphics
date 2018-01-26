c449a5aab41fb7a5f258bdb54108be923a6caec4
Michał Urbaniak <blali733@gmail.com>
2018-01-26 16:00:50 +0100

    DOCUMENTED: * mhaIO (osutils.fileIO)

--- osutils/fileIO/mhaIO.py
+++ osutils/fileIO/mhaIO.py
@@ -3,8 +3,31 @@ import numpy as np
 
 
 def load_mha(path):
+    """
+    Method loads mha file as np.array of type Int16
+
+    Parameters
+    ----------
+    path : string
+        Path to mha file
+
+    Returns
+    -------
+    np.array
+        Mha file as 3 dimensional numpy array of type Int16
+    """
     return sitk.GetArrayFromImage(sitk.ReadImage(path)).astype(np.int16)
 
 
 def save_mha(array_of_layers, path):
+    """
+    Method saves np.array to mha file
+
+    Parameters
+    ----------
+    array_of_layers : np.array
+        3d numpy array containing image to be saved
+    path : string
+        Path to output mha file.
+    """
     sitk.WriteImage(sitk.GetImageFromArray(array_of_layers), path)
