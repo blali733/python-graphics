1c5b1f40846bebe17c3816123081f441c2d8f253
Michał Urbaniak <blali733@gmail.com>
2018-01-27 15:50:40 +0100

    Rewritten mha image viewer.
    Closes #8 #11 #13

--- mhaDisplay/medImageServicing.py
+++ mhaDisplay/medImageServicing.py
@@ -1,127 +1,204 @@
-import tools.mha.mhaSlicer
 import numpy as np
 import SimpleITK as sitk
 import matplotlib.pyplot as plt
-import matplotlib.cm as cm
 import pathlib
+import os
 
 
-def med_load(path):
-    i = sitk.GetArrayFromImage(sitk.ReadImage(path))
-    return i
+class Plotter:
+    def __init__(self):
+        self.image_abs = None
+        self.image_rel = None
+        self.image_shape = None
+        self.mask = None
+        self.name = None
+        self.mask_cutoff = 0
+        self.axis = 0
+        self.layer = 0
+        self.relative = 0
+        self.overlay = 0
+        self.image_max = 0
 
-
-def med_slice(med_image, axis, slice_id):
-    return tools.mha.mhaSlicer.get_nth_slice(med_image, axis, slice_id)
-
-
-def med_plot(med_image_slice):
-    plt.imshow(med_image_slice, cmap=cm.Greys_r)
-    plt.pause(0.0001)
-
-
-def med_dual_slice(med_image, med_image2, mask_offset, axis, slice_id):
-    if axis == 0:
-        if slice_id <= med_image.shape[0]:
-            mask = med_image_binearize(med_image2[slice_id, :, :], mask_offset)
-            med_image_uint = med_2_float(med_image[slice_id, :, :])
-            return np.stack([med_image_uint,
-                             np.multiply(med_image_uint, mask),
-                             np.multiply(med_image_uint, mask)], axis=2)
+    def set_image(self, image_path):
+        image = sitk.GetArrayFromImage(sitk.ReadImage(image_path))
+        if os.name == "nt":
+            self.name = image_path.split('\\')[-1]
         else:
-            return 0
-    elif axis == 1:
-        if slice_id <= med_image.shape[1]:
-            mask = med_image_binearize(med_image2[:, slice_id, :], mask_offset)
-            med_image_uint = med_2_float(med_image[:, slice_id, :])
-            return np.stack([med_image_uint,
-                             np.multiply(med_image_uint, mask),
-                             np.multiply(med_image_uint, mask)], axis=2)
+            self.name = image_path.split('/')[-1]
+        self.image_max = image.max()
+        self.axis = 0
+        self.layer = 0
+        self.image_shape = image.shape
+        self.image_abs = []
+        self.image_abs.append(self.get_all_slices(image, 0))
+        self.image_abs.append(self.get_all_slices(image, 1))
+        self.image_abs.append(self.get_all_slices(image, 2))
+        self.image_rel = []
+        self.image_rel.append(self.get_all_slices(image, 0, False))
+        self.image_rel.append(self.get_all_slices(image, 1, False))
+        self.image_rel.append(self.get_all_slices(image, 2, False))
+        self.redraw()
+
+    def set_mask(self, image_path):
+        image = sitk.GetArrayFromImage(sitk.ReadImage(image_path))
+        if self.image_shape != image.shape:
+            raise TypeError("Mask and image size mismatch!")
+        self.overlay = 1
+        self.mask = []
+        self.mask_cutoff = 0
+        self.mask.append(self.get_all_slices(image, 0, convert=False))
+        self.mask.append(self.get_all_slices(image, 1, convert=False))
+        self.mask.append(self.get_all_slices(image, 2, convert=False))
+        self.redraw()
+
+    def unset_mask(self):
+        self.mask = None
+        self.overlay = 0
+        self.redraw()
+        
+    def redraw(self):
+        if self.overlay == 0:
+            if self.relative == 1:
+                image = self.image_rel[self.axis][self.layer]
+            else:
+                image = self.image_abs[self.axis][self.layer]
+            result = np.stack([image, image, image], axis=2)
+            plt.imshow(result)
         else:
-            return 0
-    else:
-        if slice_id <= med_image.shape[2]:
-            mask = med_image_binearize(med_image2[:, :, slice_id], mask_offset)
-            med_image_uint = med_2_float(med_image[:, :, slice_id])
-            return np.stack([med_image_uint,
-                             np.multiply(med_image_uint, mask),
-                             np.multiply(med_image_uint, mask)], axis=2)
+            mask = self.binearize(self.mask[self.axis][self.layer], self.mask_cutoff)
+            if self.relative == 1:
+                image = self.image_rel[self.axis][self.layer]
+            else:
+                image = self.image_abs[self.axis][self.layer]
+            masked_image = np.multiply(image, mask)
+            result = np.stack([image, masked_image, masked_image], axis=2)
+            plt.imshow(result)
+        plt.pause(0.0001)
+
+    def get_all_slices(self, image, axis, absolute_conversion=True, convert=True):
+        slices = []
+        if convert:
+            if absolute_conversion:
+                variant = True
+            else:
+                variant = False
+            if axis == 0:
+                for i in range(image.shape[0]):
+                    slices.append(self.int_2_float(image[i, :, :], variant))
+            elif axis == 1:
+                for i in range(image.shape[1]):
+                    slices.append(self.int_2_float(image[:, i, :], variant))
+            elif axis == 2:
+                for i in range(image.shape[2]):
+                    slices.append(self.int_2_float(image[:, :, i], variant))
         else:
-            return 0
+            if axis == 0:
+                for i in range(image.shape[0]):
+                    slices.append(image[i, :, :])
+            elif axis == 1:
+                for i in range(image.shape[1]):
+                    slices.append(image[:, i, :])
+            elif axis == 2:
+                for i in range(image.shape[2]):
+                    slices.append(image[:, :, i])
+        return slices
 
+    def toggle_overlay(self):
+        if self.mask is not None:
+            self.overlay ^= 1
+            self.redraw()
 
-def med_color_plot(med_image_slice):
-    plt.imshow(med_image_slice)
-    plt.pause(0.0001)
+    def toggle_relativity(self):
+        self.relative ^= 1
+        self.redraw()
 
+    def next_axis(self):
+        self.axis += 1
+        self.axis %= 3
+        self.layer = 0
+        self.redraw()
+        
+    def previous_axis(self):
+        self.axis -= 1
+        self.axis %= 3
+        self.layer = 0
+        self.redraw()
 
-def med_get_size(med_image, axis):
-    if axis == 0:
-        return med_image.shape[0]
-    elif axis == 1:
-        return med_image.shape[1]
-    else:
-        return med_image.shape[2]
+    def next_layer(self, offset=1):
+        if self.layer + offset < self.image_shape[self.axis]-1:
+            self.layer += offset
+        else:
+            self.layer = self.image_shape[self.axis]-1
+        self.redraw()
 
+    def prev_layer(self, offset=1):
+        if self.layer - offset > 0:
+            self.layer -= offset
+        else:
+            self.layer = 0
+        self.redraw()
 
-def med_2_csv(med_image_slice, csv_file_name):
-    pathlib.Path('./data/raw/csv').mkdir(parents=True, exist_ok=True)
-    file = open("./data/raw/csv/"+csv_file_name+".csv", "w")
-    for y in range(med_image_slice.shape[0]):
-        for x in range(med_image_slice.shape[1]):
-            file.write(med_image_slice[y, x].__str__()+"; ")
-        file.write("\n")
-    file.close()
+    def int_2_float(self, image_slice, absolute=True):
+        """
+        Converts any np.array into float format compatible with images.
 
+        Parameters
+        ----------
+        image_slice : np.array
+            Numpy array representing image
+        absolute : bool
+            Defines if scaling value should be taken as max of loaded image, or as local maximum of slice.
 
-def med_2_float(med_image_slice, relative=True):
-    """
-    Converts any np.array into float format compatible with images.
+        Returns
+        -------
+        np.array
+            normalized image.
+        """
+        if absolute:
+            max_val = max(self.image_max, 1)
+        else:
+            max_val = image_slice.max()
+            max_val = max(max_val, 1)
+        val = 1 / max_val
+        temp = image_slice.astype(np.float).copy()
+        temp *= val  # Legends say that division is longer operation.
+        return temp
 
-    Parameters
-    ----------
-    med_image_slice : np.array
-        Numpy array representing image
-    relative : bool
-        Defines if scaling value should be taken as max image value (default), or max of int16 (as stated in BRATS15
-        dataset).
+    def binearize(self, med_image_slice, level=0):
+        """
+        Function binearizing numpy arrays
 
-    Returns
-    -------
-    np.array
-        normalized image.
-    """
-    if relative:
-        max_val = med_image_slice.max()
-        max_val = max(max_val, 1)
-    else:
-        max_val = np.iinfo(np.int16).max  # Specification of dataset says, that only positive values of int16 are used.
-    # temp = np.zeros(med_image_slice.shape, dtype=np.float)
-    # for x in range(med_image_slice.shape[0] - 1):
-    #     for y in range(med_image_slice.shape[1] - 1):
-    #         temp[x, y] = med_image_slice[x, y]/max_val
-    # return temp
-    val = 1/max_val
-    temp = med_image_slice.astype(np.float).copy()
-    temp *= val  # Legends say that division is longer operation.
-    return temp
+        Parameters
+        ----------
+        med_image_slice : np.array
+            image to be binearized
+        level : int
+            level at which binearization would be performed; default 0
 
+        Returns
+        -------
+        np.array
+            binearized image as np.array
+        """
+        level = level + 0.5
+        return (med_image_slice > level) * 1
 
-def med_image_binearize(med_image_slice, level=0):
-    """
-    Function binearizing numpy arrays
+    def get_size(self, axis):
+        return self.image_shape[axis]
 
-    Parameters
-    ----------
-    med_image_slice : np.array
-        image to be binearized
-    level : int
-        level at which binearization would be performed; default 0
+    def med_2_csv(self):
+        pathlib.Path('./data/raw/csv').mkdir(parents=True, exist_ok=True)
+        name = self.name + '_' + self.axis.__str__() + '_' + self.layer.__str__()
+        file = open("./data/raw/csv/" + name + ".csv", "w")
+        if self.relative == 1:
+            image_slice = self.image_rel[self.axis][self.layer]
+        else:
+            image_slice = self.image_abs[self.axis][self.layer]
+        for x in range(self.image_shape[0]):
+            for y in range(self.image_shape[1]):
+                file.write(image_slice[x, y].__str__()+"; ")
+            file.write("\n")
+        file.close()
 
-    Returns
-    -------
-    np.array
-        binearized image as nparray
-    """
-    level = level+0.5
-    return (med_image_slice > level)*1
+    def get_info(self):
+        print("Dimensions of file:", self.image_shape)
--- mhaImageView.py
+++ mhaImageView.py
@@ -1,166 +1,74 @@
-# Windows only version, sorry
 from mhaDisplay import medImageServicing as msc
 import matplotlib.pyplot as plt
-from tools.mha import mhaMath
-import numpy as np
 try:
-    from osutils import windows as osutil
+    from mhaDisplay import windows as osutil
 except ImportError:
-    from osutils import posix as osutil
+    from mhaDisplay import posix as osutil
 
-# TODO clean this mess up
+# TODO catch and service exception on wrong files.
 
 
 class Preview:
+    def __init__(self):
+        self.imageService = msc.Plotter()
+        self.mode = 0
+        self.params = None
+        self.inner_loop_fuse = 1
+        self.main_loop_fuse = 1
+
     def main(self):
         plt.ion()
-        main_loop_fuse = 1
-        while main_loop_fuse == 1:
-            image_path = input("Image file in RAW directory: ")
-            image_full_path = "./data/raw/"+image_path
-            image_data = msc.med_load(image_full_path)
-            axis = 0
-            slice_id = 0
-            fig = msc.med_plot(msc.med_slice(image_data, axis, slice_id))
+        while self.main_loop_fuse == 1:
+            image_path = input("MHA image path: ")
+            self.imageService.set_image(image_path)
             self.my_help()
-            params = osutil.start_key_listener()
-            inner_loop_fuse = 1
-            single_mode_fuse = 1
-            dual_mode_fuse = 0
-            while inner_loop_fuse == 1:
-                while single_mode_fuse == 1:
-                    key = osutil.get_key_value()
-                    # print(key)
-                    if key == "Q":
-                        inner_loop_fuse = 0
-                        single_mode_fuse = 0
-                        osutil.stop_key_listener(params)
-                    elif key == "X":
-                        main_loop_fuse = 0
-                        inner_loop_fuse = 0
-                        single_mode_fuse = 0
-                        osutil.stop_key_listener(params)
-                    elif key == "4":
-                        if slice_id > 1:
-                            slice_id -= 1
-                            msc.med_plot(msc.med_slice(image_data, axis, slice_id))
-                    elif key == "6":
-                        if slice_id < msc.med_get_size(image_data, axis) - 2:
-                            slice_id += 1
-                            msc.med_plot(msc.med_slice(image_data, axis, slice_id))
-                    elif key == "7":
-                        if slice_id > 5:
-                            slice_id -= 5
-                            msc.med_plot(msc.med_slice(image_data, axis, slice_id))
-                        else:
-                            slice_id = 0
-                            msc.med_plot(msc.med_slice(image_data, axis, slice_id))
-                    elif key == "9":
-                        if slice_id < msc.med_get_size(image_data, axis) - 6:
-                            slice_id += 5
-                            msc.med_plot(msc.med_slice(image_data, axis, slice_id))
-                        else:
-                            slice_id = msc.med_get_size(image_data, axis)-1
-                            msc.med_plot(msc.med_slice(image_data, axis, slice_id))
-                    elif key == "8":
-                        slice_id = 0
-                        if axis < 2:
-                            axis += 1
-                        msc.med_plot(msc.med_slice(image_data, axis, slice_id))
-                    elif key == "2":
-                        slice_id = 0
-                        if axis > 0:
-                            axis -= 1
-                        msc.med_plot(msc.med_slice(image_data, axis, slice_id))
-                    elif key == "5":
-                        print(image_data.shape, image_data.dtype)
-                        print(image_data.max(), image_data.min())
-                    elif key == "3":
-                        print("Saving file in csv format...")
-                        msc.med_2_csv(msc.med_slice(image_data, axis, slice_id), image_path+"_"+axis.__str__()+"_"+slice_id.__str__())
-                        print("File saved.")
-                    elif key == "1":
-                        # Switch to dual mode:
-                        single_mode_fuse = 0
-                        dual_mode_fuse = 1
-                    elif key == "0":
-                        t_slice = mhaMath.med_2_float(msc.med_slice(image_data, axis, slice_id))
-                        msc.med_color_plot(np.stack((t_slice, t_slice, t_slice), axis=2))
-                while dual_mode_fuse == 1:
-                    osutil.stop_key_listener(params)
-                    image2_path = input("Second image file in RAW directory: ")
-                    image2_full_path = "./data/raw/" + image2_path
-                    image2_data = msc.med_load(image2_full_path)
-                    mask_cutoff = 0
-                    osutil.start_key_listener()
-                    msc.med_color_plot(
-                        msc.med_dual_slice(image_data, image2_data, mask_cutoff, axis, slice_id))
-                    while dual_mode_fuse == 1:
-                        key = osutil.get_key_value()
-                        if key == "Q":
-                            dual_mode_fuse = 0
-                            single_mode_fuse = 1
-                            # osutil.stop_key_listener(params)
-                        elif key == "X":
-                            dual_mode_fuse = 0
-                            main_loop_fuse = 0
-                            inner_loop_fuse = 0
-                            osutil.stop_key_listener(params)
-                        elif key == "4":
-                            if slice_id > 1:
-                                slice_id -= 1
-                                msc.med_color_plot(
-                                    msc.med_dual_slice(image_data, image2_data, mask_cutoff, axis, slice_id))
-                        elif key == "6":
-                            if slice_id < msc.med_get_size(image_data, axis) - 2:
-                                slice_id += 1
-                                msc.med_color_plot(
-                                    msc.med_dual_slice(image_data, image2_data, mask_cutoff, axis, slice_id))
-                        elif key == "7":
-                            if slice_id > 5:
-                                slice_id -= 5
-                                msc.med_color_plot(
-                                    msc.med_dual_slice(image_data, image2_data, mask_cutoff, axis, slice_id))
-                            else:
-                                slice_id = 0
-                                msc.med_color_plot(
-                                    msc.med_dual_slice(image_data, image2_data, mask_cutoff, axis, slice_id))
-                        elif key == "9":
-                            if slice_id < msc.med_get_size(image_data, axis) - 6:
-                                slice_id += 5
-                                msc.med_color_plot(
-                                    msc.med_dual_slice(image_data, image2_data, mask_cutoff, axis, slice_id))
-                            else:
-                                slice_id = msc.med_get_size(image_data, axis) - 1
-                                msc.med_color_plot(
-                                    msc.med_dual_slice(image_data, image2_data, mask_cutoff, axis, slice_id))
-                        elif key == "8":
-                            slice_id = 0
-                            if axis < 2:
-                                axis += 1
-                            msc.med_color_plot(
-                                msc.med_dual_slice(image_data, image2_data, mask_cutoff, axis, slice_id))
-                        elif key == "2":
-                            slice_id = 0
-                            if axis > 0:
-                                axis -= 1
-                            msc.med_color_plot(
-                                msc.med_dual_slice(image_data, image2_data, mask_cutoff, axis, slice_id))
-                        elif key == "1":
-                            msc.med_plot(msc.med_slice(image_data, axis, slice_id))
-                        elif key == "0":
-                            msc.med_color_plot(
-                                msc.med_dual_slice(image_data, image2_data, mask_cutoff, axis, slice_id))
-                        elif key == "-":
-                            if mask_cutoff > 0:
-                                mask_cutoff -= 1
-                                msc.med_color_plot(
-                                    msc.med_dual_slice(image_data, image2_data, mask_cutoff, axis, slice_id))
-                        elif key == "+":
-                            if mask_cutoff < image2_data.max():
-                                mask_cutoff += 1
-                                msc.med_color_plot(
-                                    msc.med_dual_slice(image_data, image2_data, mask_cutoff, axis, slice_id))
+            self.params = osutil.start_key_listener()
+            self.inner_loop_fuse = 1
+            while self.inner_loop_fuse == 1:
+                key = osutil.get_key_value()
+                self.service_key(key)
+
+    def service_key(self, key):
+        if key == "9":
+            self.imageService.next_layer(5)
+        elif key == "6":
+            self.imageService.next_layer()
+        elif key == "7":
+            self.imageService.prev_layer(5)
+        elif key == "4":
+            self.imageService.prev_layer()
+        elif key == "8":
+            self.imageService.next_axis()
+        elif key == "2":
+            self.imageService.previous_axis()
+        elif key == "5":
+            self.imageService.get_info()
+        elif key == "3":
+            self.imageService.med_2_csv()
+        elif key == "1":
+            if self.mode == 1:
+                self.imageService.toggle_overlay()
+            else:
+                try:
+                    osutil.stop_key_listener(self.params)
+                    path = input("Please give path to mask in MHA format: ")
+                    self.imageService.set_mask(path)
+                    self.params = osutil.start_key_listener()
+                    self.mode = 1
+                except TypeError as e:
+                    print(e.args[0])
+        elif key == "Q":
+            if self.mode == 1:
+                self.imageService.unset_mask()
+                self.mode = 0
+            else:
+                self.inner_loop_fuse = 0
+        elif key == "X":
+            self.main_loop_fuse = 0
+            self.inner_loop_fuse = 0
+        elif key == "c":
+            self.imageService.toggle_relativity()
+
                         
     def my_help(self):
         print("4, 7 - navigate left")
@@ -168,7 +76,7 @@ class Preview:
         print("2, 8 - change axis")
         print("5 - File info")
         print("3 - save current slice to csv file")
-        print("0 - show real colors")
+        print("c - toggle relativity mode")
         print("1 - enable mask overlay:")
         print("    1 - hide overlay")
         print("    0 - show overlay")
