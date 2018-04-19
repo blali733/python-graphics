ba8b1096b695b033dc3e7e85c0f2e30b215da53c
Michał Urbaniak <blali733@gmail.com>
2018-04-19 12:21:27 +0200

    Added test of resize for resize function.

--- test/data_generators.py
+++ test/data_generators.py
@@ -33,7 +33,46 @@ def gen_scalable_matrix():
     return mat
 
 
-def gen_scaled_up_2_matrix():
+def gen_scaled_down_2y_matrix():
+    mat = np.zeros([3, 6], dtype=np.uint16)
+    mat[0, 2:4] = 1
+    mat[1, 0:2] = 1
+    mat[0, 4:6] = 2
+    mat[1, 2:4] = 2
+    mat[2, 0:2] = 2
+    mat[1, 4:6] = 3
+    mat[2, 2:4] = 3
+    mat[2, 4:6] = 4
+    return mat
+
+
+def gen_scaled_down_2x_matrix():
+    mat = np.zeros([6, 3], dtype=np.uint16)
+    mat[0:2, 1] = 1
+    mat[2:4, 0] = 1
+    mat[0:2, 2] = 2
+    mat[2:4, 1] = 2
+    mat[4:6, 0] = 2
+    mat[2:4, 2] = 3
+    mat[4:6, 1] = 3
+    mat[4:6, 2] = 4
+    return mat
+
+
+def gen_scaled_down_2xy_matrix():
+    mat = np.zeros([3, 3], dtype=np.uint16)
+    mat[0, 1] = 1
+    mat[1, 0] = 1
+    mat[0, 2] = 2
+    mat[1, 1] = 2
+    mat[2, 0] = 2
+    mat[1, 2] = 3
+    mat[2, 1] = 3
+    mat[2, 2] = 4
+    return mat
+
+
+def gen_scaled_up_2xy_matrix():
     mat = np.zeros([12, 12], dtype=np.uint16)
     mat[0:4, 4:8] = 1
     mat[4:8, 0:4] = 1
@@ -46,14 +85,27 @@ def gen_scaled_up_2_matrix():
     return mat
 
 
-def gen_scaled_down_2_matrix():
-    mat = np.zeros([3, 3], dtype=np.uint16)
-    mat[0, 1] = 1
-    mat[1, 0] = 1
-    mat[0, 2] = 2
-    mat[1, 1] = 2
-    mat[2, 0] = 2
-    mat[1, 2] = 3
-    mat[2, 1] = 3
-    mat[2, 2] = 4
+def gen_scaled_up_2x_matrix():
+    mat = np.zeros([6, 12], dtype=np.uint16)
+    mat[0:2, 4:8] = 1
+    mat[2:4, 0:4] = 1
+    mat[0:2, 8:12] = 2
+    mat[2:4, 4:8] = 2
+    mat[4:6, 0:4] = 2
+    mat[2:4, 8:12] = 3
+    mat[4:6, 4:8] = 3
+    mat[4:6, 8:12] = 4
+    return mat
+
+
+def gen_scaled_up_2y_matrix():
+    mat = np.zeros([12, 6], dtype=np.uint16)
+    mat[0:4, 2:4] = 1
+    mat[4:8, 0:2] = 1
+    mat[0:4, 4:6] = 2
+    mat[4:8, 2:4] = 2
+    mat[8:12, 0:2] = 2
+    mat[4:8, 4:6] = 3
+    mat[8:12, 2:4] = 3
+    mat[8:12, 4:6] = 4
     return mat
--- test/tools/matrix/test_resizer.py
+++ test/tools/matrix/test_resizer.py
@@ -6,32 +6,62 @@ from src.tools.matrix import resizer
 class TestResizer:
     def test_imresize_downscale(self):
         base = dg.gen_scalable_matrix()
-        expected = dg.gen_scaled_down_2_matrix()
+        expected = dg.gen_scaled_down_2xy_matrix()
         assert np.array_equal(expected, resizer.imresize(base, (3, 3)))
 
     def test_imresize_upscale(self):
         base = dg.gen_scalable_matrix()
-        expected = dg.gen_scaled_up_2_matrix()
+        expected = dg.gen_scaled_up_2xy_matrix()
         assert np.array_equal(expected, resizer.imresize(base, (12, 12)))
 
     def test_resize_1_to_1(self):
         base = dg.gen_matrix(dtype=np.uint16)
         assert np.array_equal(base, resizer.resize(base, 7, 7))
 
-    def test_resize_no_upscale_xy_fill(self):
+    def test_resize_up_xy_fill(self):
         base = dg.gen_matrix(dtype=np.uint16)
         result = np.pad(base, ((4, 4), (4, 4)), mode="constant")
         assert np.array_equal(result, resizer.resize(base, 15, 15))
 
-    def test_resize__no_upscale_x_fill(self):
+    def test_resize_up_x_fill(self):
         base = dg.gen_matrix(dtype=np.uint16)
         result = np.pad(base, ((0, 0), (4, 4)), mode="constant")
         assert np.array_equal(result, resizer.resize(base, 7, 15))
 
-    def test_resize__no_upscale_y_fill(self):
+    def test_resize_up_y_fill(self):
         base = dg.gen_matrix(dtype=np.uint16)
         result = np.pad(base, ((4, 4), (0, 0)), mode="constant")
         assert np.array_equal(result, resizer.resize(base, 15, 7))
+
+    def test_imresize_up_xy_stretch(self):
+        base = dg.gen_scalable_matrix()
+        expected = dg.gen_scaled_up_2xy_matrix()
+        assert np.array_equal(expected, resizer.resize(base, 12, upscale=True))
+
+    def test_imresize_up_x_stretch(self):
+        base = dg.gen_scalable_matrix()
+        expected = dg.gen_scaled_up_2x_matrix()
+        assert np.array_equal(expected, resizer.resize(base, 12, 6, upscale=True))
+
+    def test_imresize_up_y_stretch(self):
+        base = dg.gen_scalable_matrix()
+        expected = dg.gen_scaled_up_2y_matrix()
+        assert np.array_equal(expected, resizer.resize(base, 6, 12, upscale=True))
+
+    def test_resize_down_xy(self):
+        base = dg.gen_scalable_matrix()
+        expected = dg.gen_scaled_down_2xy_matrix()
+        assert np.array_equal(expected, resizer.resize(base, 3))
+
+    def test_resize_down_x(self):
+        base = dg.gen_scalable_matrix()
+        expected = dg.gen_scaled_down_2x_matrix()
+        assert np.array_equal(expected, resizer.resize(base, 3, 6))
+
+    def test_resize_down_y(self):
+        base = dg.gen_scalable_matrix()
+        expected = dg.gen_scaled_down_2y_matrix()
+        assert np.array_equal(expected, resizer.resize(base, 6, 3))
 
     # def test_shrink(self):
     #     assert False
