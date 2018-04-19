Tested shrinking and expanding in matrix resizer. Fixed wrong offset calculation in absolute mode of shrink.

src/tools/matrix/resizer.py
@@ -99,15 +99,15 @@ def shrink(mask, origin, size, absolute=False):
     # Just in case:
     if origin[0] < 0 or origin[1] < 0 or size[0] < 0 or size[1] < 0:
         raise IndexError("Parameters can not be negative!")
-    x = origin[0]
-    y = origin[1]
+    d1 = origin[0]
+    d2 = origin[1]
     if absolute:
-        mx = size[0]
-        my = size[1]
+        md1 = size[0] + 1
+        md2 = size[1] + 1
     else:
-        mx = x + size[0]
-        my = y + size[1]
-    return mask[x:mx, y:my]
+        md1 = d1 + size[0]
+        md2 = d2 + size[1]
+    return mask[d1:md1, d2:md2]
 
 
 def expand(mask, origin, size, desired_size):
@@ -133,10 +133,10 @@ def expand(mask, origin, size, desired_size):
     # Just in case:
     if origin[0] < 0 or origin[1] < 0 or size[0] < 0 or size[1] < 0 or desired_size[0] < 0 or desired_size[1] < 0:
         raise IndexError("Parameters can not be negative!")
-    x = origin[0]
-    y = origin[1]
-    mx = x + size[0]
-    my = y + size[1]
-    if desired_size[0]-mx < 0 or desired_size[1]-my < 0:
+    d1 = origin[0]
+    d2 = origin[1]
+    md1 = d1 + size[0]
+    md2 = d2 + size[1]
+    if desired_size[0]-md1 < 0 or desired_size[1]-md2 < 0:
         raise IndexError("Cannot expand to smaller container!")
-    return np.pad(mask, ([x, desired_size[0]-mx], [y, desired_size[1]-my]), mode="constant")
+    return np.pad(mask, ([d1, desired_size[0]-md1], [d2, desired_size[1]-md2]), mode="constant")

test/data_generators.py
@@ -23,89 +23,89 @@ def gen_matrix(dtype):
 def gen_scalable_matrix():
     mat = np.zeros([6, 6], dtype=np.uint16)
     mat[0:2, 2:4] = 1
-    mat[2:4, 0:2] = 1
     mat[0:2, 4:6] = 2
-    mat[2:4, 2:4] = 2
-    mat[4:6, 0:2] = 2
-    mat[2:4, 4:6] = 3
-    mat[4:6, 2:4] = 3
-    mat[4:6, 4:6] = 4
+    mat[2:4, 0:2] = 3
+    mat[2:4, 2:4] = 4
+    mat[2:4, 4:6] = 5
+    mat[4:6, 0:2] = 6
+    mat[4:6, 2:4] = 7
+    mat[4:6, 4:6] = 8
     return mat
 
 
 def gen_scaled_down_2y_matrix():
     mat = np.zeros([3, 6], dtype=np.uint16)
     mat[0, 2:4] = 1
-    mat[1, 0:2] = 1
     mat[0, 4:6] = 2
-    mat[1, 2:4] = 2
-    mat[2, 0:2] = 2
-    mat[1, 4:6] = 3
-    mat[2, 2:4] = 3
-    mat[2, 4:6] = 4
+    mat[1, 0:2] = 3
+    mat[1, 2:4] = 4
+    mat[1, 4:6] = 5
+    mat[2, 0:2] = 6
+    mat[2, 2:4] = 7
+    mat[2, 4:6] = 8
     return mat
 
 
 def gen_scaled_down_2x_matrix():
     mat = np.zeros([6, 3], dtype=np.uint16)
     mat[0:2, 1] = 1
-    mat[2:4, 0] = 1
     mat[0:2, 2] = 2
-    mat[2:4, 1] = 2
-    mat[4:6, 0] = 2
-    mat[2:4, 2] = 3
-    mat[4:6, 1] = 3
-    mat[4:6, 2] = 4
+    mat[2:4, 0] = 3
+    mat[2:4, 1] = 4
+    mat[2:4, 2] = 5
+    mat[4:6, 0] = 6
+    mat[4:6, 1] = 7
+    mat[4:6, 2] = 8
     return mat
 
 
 def gen_scaled_down_2xy_matrix():
     mat = np.zeros([3, 3], dtype=np.uint16)
     mat[0, 1] = 1
-    mat[1, 0] = 1
     mat[0, 2] = 2
-    mat[1, 1] = 2
-    mat[2, 0] = 2
-    mat[1, 2] = 3
-    mat[2, 1] = 3
-    mat[2, 2] = 4
+    mat[1, 0] = 3
+    mat[1, 1] = 4
+    mat[1, 2] = 5
+    mat[2, 0] = 6
+    mat[2, 1] = 7
+    mat[2, 2] = 8
     return mat
 
 
 def gen_scaled_up_2xy_matrix():
     mat = np.zeros([12, 12], dtype=np.uint16)
     mat[0:4, 4:8] = 1
-    mat[4:8, 0:4] = 1
     mat[0:4, 8:12] = 2
-    mat[4:8, 4:8] = 2
-    mat[8:12, 0:4] = 2
-    mat[4:8, 8:12] = 3
-    mat[8:12, 4:8] = 3
-    mat[8:12, 8:12] = 4
+    mat[4:8, 0:4] = 3
+    mat[4:8, 4:8] = 4
+    mat[4:8, 8:12] = 5
+    mat[8:12, 0:4] = 6
+    mat[8:12, 4:8] = 7
+    mat[8:12, 8:12] = 8
     return mat
 
 
 def gen_scaled_up_2x_matrix():
     mat = np.zeros([6, 12], dtype=np.uint16)
     mat[0:2, 4:8] = 1
-    mat[2:4, 0:4] = 1
     mat[0:2, 8:12] = 2
-    mat[2:4, 4:8] = 2
-    mat[4:6, 0:4] = 2
-    mat[2:4, 8:12] = 3
-    mat[4:6, 4:8] = 3
-    mat[4:6, 8:12] = 4
+    mat[2:4, 0:4] = 3
+    mat[2:4, 4:8] = 4
+    mat[2:4, 8:12] = 5
+    mat[4:6, 0:4] = 6
+    mat[4:6, 4:8] = 7
+    mat[4:6, 8:12] = 8
     return mat
 
 
 def gen_scaled_up_2y_matrix():
     mat = np.zeros([12, 6], dtype=np.uint16)
     mat[0:4, 2:4] = 1
-    mat[4:8, 0:2] = 1
     mat[0:4, 4:6] = 2
-    mat[4:8, 2:4] = 2
-    mat[8:12, 0:2] = 2
-    mat[4:8, 4:6] = 3
-    mat[8:12, 2:4] = 3
-    mat[8:12, 4:6] = 4
+    mat[4:8, 0:2] = 3
+    mat[4:8, 2:4] = 4
+    mat[4:8, 4:6] = 5
+    mat[8:12, 0:2] = 6
+    mat[8:12, 2:4] = 7
+    mat[8:12, 4:6] = 8
     return mat