bbbff827345b23e852e2d5c94bc52ba798575325
Michał Urbaniak <blali733@gmail.com>
2018-04-20 12:11:45 +0200

    Clean up of unused code.

--- src/tools/matrix/recreate.py
+++ src/tools/matrix/recreate.py
@@ -19,26 +19,6 @@ def create_3d_array(layers_list, axis):
     """
     # TODO check if this works properly.
     array_3d = np.stack(layers_list, axis=axis)
-    # dtype = layers_list[0].dtype
-    # shape = layers_list[0].shape
-    # if axis == 0:
-    #     # array_3d = np.zeros((layers_list.__len__(), shape[0], shape[1]), dtype=dtype)
-    #     # layer_id = 0
-    #     # for layer in layers_list:
-    #     #     array_3d[layer_id, :, :] = layer
-    #     #     layer_id += 1
-    # elif axis == 1:
-    #     array_3d = np.zeros((shape[0], layers_list.__len__(), shape[1]), dtype=dtype)
-    #     layer_id = 0
-    #     for layer in layers_list:
-    #         array_3d[:, layer_id, :] = layer
-    #         layer_id += 1
-    # else:
-    #     array_3d = np.zeros((shape[0], shape[1], layers_list.__len__()), dtype=dtype)
-    #     layer_id = 0
-    #     for layer in layers_list:
-    #         array_3d[:, :, layer_id] = layer
-    #         layer_id += 1
     return array_3d
 
 
